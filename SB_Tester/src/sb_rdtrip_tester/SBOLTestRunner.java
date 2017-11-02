0371b5de48a43dacfb158453ecd6337b71758012
Meher <mehersam251@gmail.com>
2017-11-02 16:58:42 -0600

    updated testrunner

--- SB_Tester/src/sb_rdtrip_tester/SBOLTestRunner.java
+++ SB_Tester/src/sb_rdtrip_tester/SBOLTestRunner.java
@@ -1,5 +1,18 @@
 package sb_rdtrip_tester;
 
+import java.io.File;
+import java.net.URI;
+
+import org.sbolstandard.core2.SBOLDocument;
+import org.synbiohub.frontend.SynBioHubFrontend;
+
 public class SBOLTestRunner {
 
+	private SynBioHubFrontend frontend; 
+	private SBOLDocument doc;
+
+	public SBOLTestRunner()
+	{
+		
+	}
 }
